bfce0f347d1a0ee346eb30a9e571d96551d5ba78
Paul M. Lyon <pmlyon@ifai.com>
2018-08-28 15:08:13 -0500

    Added columns to transactions table

---
+++ database/migrations/2018_08_28_190749_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->date('transaction_date');
+            $table->decimal('dollar_amount');
+            $table->string('category');
+            $table->boolean('repeat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
