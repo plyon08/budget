8c50c1c65fe2de70c3f4cf1a2fc2cb4b1be46bb2
Paul M. Lyon <pmlyon@ifai.com>
2018-08-31 16:48:18 -0500

    Finished index, create, and store methods for routes.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,18 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Transaction;
+use App\User;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class TransactionController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $category = null;
+        $month = null;
+        $year = null;
+        $query = Transaction::orderBy('transaction_date', 'desc');
+
+        if (!empty($request->input('category'))) {
+            $category = $request->input('category');
+            $query->category($category);
+        }
+        if (!empty($request->input('month'))) {
+            $month = $request->input('month');
+            $query->month($month);
+        }
+        if (!empty($request->input('year'))) {
+            $year = $request->input('year');
+            $query->year($year);
+        }
+        
+        $transactions = $query->get();
+
+        return view('index', compact('transactions'));
     }
 
     /**
@@ -24,7 +52,7 @@ class TransactionController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
 
     /**
@@ -35,7 +63,21 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'transaction_date' => 'required|date',
+            'dollar_amount' => 'required|numeric'
+        ]);
+
+        $transaction = new Transaction;
+
+        $transaction->transaction_date = request('transaction_date');
+        $transaction->dollar_amount = request('dollar_amount');
+        $transaction->category = request('category');
+        $transaction->save();
+
+        session()->flash('message', 'Transaction Saved Successfully!');
+
+        return redirect()->route('index');
     }
 
     /**
