eb83a2723ebfce3b3f3eddaeee3b20a890afd5da
Paul M. Lyon <pmlyon@ifai.com>
2018-08-31 16:45:18 -0500

    Created main http routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Auth::routes();
 
+Route::get('/transactions', 'TransactionController@index')->name('index');
+Route::get('/transactions/add-transaction', 'TransactionController@create')->name('create');
+Route::post('/transactions', 'TransactionController@store')->name('store');
+Route::get('/transations/{transaction}', 'TransactionController@show')->name('show');
+Route::get('/transactions/{transaction}/edit-transaction', 'TransactionController@edit')->name('edit');
+Route::patch('/transactions/{transaction}', 'TransactionController@update')->name('update');
+Route::delete('/transactions/{transaction}', 'TransactionController@destroy')->name('destroy');
 Route::get('/home', 'HomeController@index')->name('home');
