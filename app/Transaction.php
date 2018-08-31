d80a990f8c211de3ed30394d85438b38ed5064f4
Paul M. Lyon <pmlyon@ifai.com>
2018-08-31 16:49:17 -0500

    Made local query scopes for Transaction class index method.

--- app/Transaction.php
+++ app/Transaction.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Transaction extends Model
 {
-    //
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['transaction_date'];
+    
+
+    public function scopeCategory($query, $category)
+    {
+        return $query->where('category', $category);
+    }
+
+    public function scopeMonth($query, $month)
+    {
+        $query->where('transaction_date', 'like', "%-$month-%");
+    }
+
+    public function scopeYear($query, $year)
+    {
+        return $query->where('transaction_date', 'like', "$year%");
+    }
 }
